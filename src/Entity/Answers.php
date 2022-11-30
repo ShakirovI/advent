e5938cbb75350d93eb934be4d1adb87e04dd5c3e
ivan shakirov <ivan.v.shakirov@gmail.com>
2022-12-01 01:59:14 +0700

    Сделал две сущности для вопросов и ответов

---
+++ src/Entity/Answers.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnswersRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnswersRepository::class)]
+class Answers
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $answerText = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAnswerText(): ?string
+    {
+        return $this->answerText;
+    }
+
+    public function setAnswerText(?string $answerText): self
+    {
+        $this->answerText = $answerText;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Questions.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuestionsRepository::class)]
+class Questions
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $questionText = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuestionText(): ?string
+    {
+        return $this->questionText;
+    }
+
+    public function setQuestionText(string $questionText): self
+    {
+        $this->questionText = $questionText;
+
+        return $this;
+    }
+}
