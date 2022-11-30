9ca0becaeb22478b68afd76c9e0bf527bc2ad47f
ivan shakirov <ivan.v.shakirov@gmail.com>
2022-12-01 02:00:39 +0700

    добавил контроллер

---
+++ src/Controller/AnswerController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Answers;
+use App\Entity\Questions;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AnswerController extends AbstractController
+{
+    public function answer(ManagerRegistry $doctrine, int $id = 0): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $answer = $entityManager->getRepository(Answers::class)->find($id);
+        $question = $entityManager->getRepository(Questions::class)->find($id);
+        return $this->render('answer/result.html.twig', [
+            'questionText' => $question,
+            'answerText' => $answer,
+        ]);
+    }
+}
